67348e972c70166b1f352821c0211a4a0157332c
CodeZeala <Antonyoy@mail.ru>
2023-02-06 21:35:15 +0300

    feat: add func for adding to card

--- sayhi.go
+++ sayhi.go
@@ -11,12 +11,18 @@ type Product struct {
 
 func main() {
 	addToCard(Product{"Трусы", "Белый", 100, 1}, 10)
+	addToCard(Product{"Футболка", "Черный", 200, 10}, 5)
 }
 
 // TODO: Сделать функцию addToCard
 func addToCard(currentProduct Product, count uint16) {
-	fmt.Println(currentProduct)
-	fmt.Println(count)
+	if currentProduct.Quantity < count {
+		var result uint16 = count - currentProduct.Quantity
+		fmt.Printf("Не хватает %d ед. товара \"%s\" \n", result, currentProduct.Name)
+	} else {
+		var sum uint16 = currentProduct.Price * count
+		fmt.Printf("Вы заказали %d ед. товара '%s' на сумму %d \n", count, currentProduct.Name, sum)
+	}
 }
 
 // test

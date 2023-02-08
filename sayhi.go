d7da28436da027ba2025ff7f5ab1e32f0f195269
CodeZeala <Antonyoy@mail.ru>
2023-02-08 21:07:10 +0300

    feat: test method and massive

--- sayhi.go
+++ sayhi.go
@@ -12,6 +12,21 @@ type Product struct {
 func main() {
 	addToCard(Product{"Трусы", "Белый", 100, 1}, 10)
 	addToCard(Product{"Футболка", "Черный", 200, 10}, 5)
+	testProduct2 := Product{
+		Name:     "Футболка",
+		Color:    "Черный",
+		Price:    200,
+		Quantity: 10,
+	}
+	testProduct3 := Product{
+		Name:     "Трусы",
+		Color:    "Белый",
+		Price:    100,
+		Quantity: 1,
+	}
+	testProduct2.method()
+	testProduct3.method()
+	massive()
 }
 
 // TODO: Сделать функцию addToCard
@@ -26,3 +41,25 @@ func addToCard(currentProduct Product, count uint16) {
 }
 
 // test1
+func (testProduct Product) method() {
+	if testProduct.Quantity == 10 {
+		fmt.Println("Это Футболка Черный")
+	} else {
+		fmt.Println("Такого товара не существует")
+	}
+}
+func massive() {
+	var a string
+	var b string
+	var prod [4]string
+	prod[0] = "Name"
+	prod[1] = "Color"
+	prod[2] = "Price"
+	prod[3] = "Quantity"
+	products := [...]string{"productName", "productColor", "productPrice", "productQuantity"}
+	a = products[3]
+	// products[5] = "productCount"
+	fmt.Println(a)
+	b = prod[0]
+	fmt.Println(b)
+}
